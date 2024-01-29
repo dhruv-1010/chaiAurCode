1ac496959774e58e0b1e4be911669644ba5e13df
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2024-01-29 06:14:23 +0000

    Complete till react router

--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -2,8 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./RouterComponents/Home";
+import About from "./RouterComponents/About";
+import Contact from "./RouterComponents/Contact";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>,
+  ),
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
